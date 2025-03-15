b4c35a0f1f206f9767a9288ef272487e3d0a588f
Nahid1828 <isam15-5334@diu.edu.bd>
2025-03-15 11:38:39 +0600

    product_Cart

--- lib/common/widgets/appbar.dart
+++ lib/common/widgets/appbar.dart
@@ -3,15 +3,27 @@ import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
 
 class FAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const FAppBar({super.key});
+  const FAppBar({
+    super.key,
+    required this.title,
+    this.firstIcon = Iconsax.notification_bing_outline,
+    this.secondIcon = Icons.shopping_cart,
+    this.backButton = false,
+  });
+
+  final String title;
+  final IconData firstIcon;
+  final IconData secondIcon;
+  final bool backButton;
 
   @override
   Size get preferredSize => Size.fromHeight(kToolbarHeight);
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      leading: backButton ? Icon(Iconsax.arrow_left_2_outline) : null,
       title: Text(
-        "Mega Shop",
+        title,
         style: TextStyle(
           fontFamily: "DMSans",
           fontWeight: FontWeight.bold,
@@ -21,12 +33,12 @@ class FAppBar extends StatelessWidget implements PreferredSizeWidget {
       centerTitle: true,
       actions: [
         Container(
-          padding: EdgeInsets.all(10),
-          child: Icon(Iconsax.notification_bing_outline),
+          padding: EdgeInsets.fromLTRB(10, 10, 5, 10),
+          child: Icon(firstIcon, color: FColors.dark),
         ),
         Container(
-          padding: EdgeInsets.all(10),
-          child: Icon(Icons.shopping_cart),
+          padding: EdgeInsets.fromLTRB(5, 10, 10, 10),
+          child: Icon(secondIcon, color: FColors.dark),
         ),
       ],
     );
--- lib/screens/home/page/home.dart
+++ lib/screens/home/page/home.dart
@@ -6,6 +6,49 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: FAppBar());
+    return Scaffold(
+      appBar: FAppBar(title: "New Title", backButton: true),
+      body: Center(
+        child: Container(
+          width: double.infinity, // Make the container take full width
+          height: 400, // Set a height for the container to ensure it is visible
+          padding: const EdgeInsets.all(16), // Padding inside the container
+          decoration: BoxDecoration(
+            color: Colors.white, // Set a background color for the container
+            borderRadius: BorderRadius.circular(10), // Rounded corners
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.3),
+                blurRadius: 10,
+                offset: Offset(0, 5),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/headphone.png', width: 200),
+              const SizedBox(height: 10),
+              const Text(
+                "TMA-2 HD Wireless",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const Text(
+                "Rp. 1.500.000",
+                style: TextStyle(fontSize: 16, color: Colors.red),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.star, color: Colors.amber),
+                  SizedBox(width: 5),
+                  Text("4.6  86 Reviews"),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
