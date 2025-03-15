0efe9cf9933b65da854133dbb215ed5da6b99d8d
Nahid1828 <isam15-5334@diu.edu.bd>
2025-03-15 09:52:35 +0600

    separate_AppBar

---
+++ lib/common/widgets/appbar.dart
@@ -0,0 +1,34 @@
+import 'package:ecommerce_nahid/utils/constants/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+
+class FAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const FAppBar({super.key});
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        "Mega Shop",
+        style: TextStyle(
+          fontFamily: "DMSans",
+          fontWeight: FontWeight.bold,
+          color: FColors.oceanBlue,
+        ),
+      ),
+      centerTitle: true,
+      actions: [
+        Container(
+          padding: EdgeInsets.all(10),
+          child: Icon(Iconsax.notification_bing_outline),
+        ),
+        Container(
+          padding: EdgeInsets.all(10),
+          child: Icon(Icons.shopping_cart),
+        ),
+      ],
+    );
+  }
+}
